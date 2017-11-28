e591312c5faecf430011cf9203ed292a46cee00f
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-27 23:03:30 -0600

    Edited signup() in main.py to check for a blank username field. If blank, a redirect will occur to show the error in the signup.html page, which has jinja if statements to check for a thrown error.

--- main.py
+++ main.py
@@ -13,12 +13,26 @@ def signup():
     verify = request.form['verify']
     email = request.form['email']
 
+    redirect_args = "/"
 
+    if (not username) or (username.strip() == ""):
+        username_error = "Username cannot be left blank."
+        redirect_args += "?username_error=" + username_error
 
-    return
+    return redirect(redirect_args)
 
 @app.route('/')
 def index():
-    return render_template('signup.html', title="User Signup", heading="Signup")
+    # username = request.args.get("username")
+    # email = request.args.get("email")
+
+    username_error = request.args.get("username_error")
+    # password_error = request.args.get("password_error")
+    # verify_error = request.args.get("verify_error")
+    # email_error = request.args.get("email_error")
+
+    return render_template("signup.html", title="User Signup", heading="Signup", username_error=username_error and cgi.escape(username_error, quote=True))
 
 app.run()
+# , username_error=username_error  and cgi.escape(username_error, quote=True)
+# email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error
